aacef6885d734985ac431a90d8864950aac97f2f
Andressa Jovino S <andressajovinos@gmail.com>
2021-03-19 19:16:53 -0300

    Revisão POO

---
+++ Pagamentos/PagamentoPIX.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagamentos
+{
+	public class PagamentoPix : Pagamento
+	{
+        public Chave Chave { get; set; }
+
+        public override string ValidaValor()
+        {
+            return base.ValidaValor() + "Agora estou dentro do pagamento Pix!";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nChave: " + this.Chave;
+        }
+    }
+}
---
+++ RevisaoOO/TestProtect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pagamentos;
+
+namespace RevisaoOO
+{
+	class TestProtect : PagamentoPix
+	{
+		public void Main()
+		{
+			this.ELegal();
+		}
+	}
+}
